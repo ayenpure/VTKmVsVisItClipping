8439b99e0ab6942532b7fb12cbbd37d11aad656d
ayenpure <ayenpure@uoregon.edu>
2018-01-25 09:32:43 -0800

    updating plot script

--- plotscript.R
+++ plotscript.R
@@ -1,15 +1,12 @@
 library("ggplot2")
-visitdata <- read.csv(file="~/visitdata.csv", head=TRUE, sep=",")
-vtkmdata <- read.csv(file="~/vtkmdata.csv", head=TRUE, sep=",")
+visitdata <- read.csv(file="/home/abhishek/repositories/VTKmVsVisItClipping/visitfile.csv", head=TRUE, sep=",")
+vtkmdata <- read.csv(file="/home/abhishek/repositories/VTKmVsVisItClipping/vtkmfile.csv", head=TRUE, sep=",")
 
 visitrows <- nrow(visitdata)
 vtkmrows <- nrow(vtkmdata)
 
 expected <- visitrows + vtkmrows
-combine <- rbind(visitrows, vtkmrows)
+combine <- rbind(visitdata, vtkmdata)
 
-if (expected == nrow(combine))
-{
-  ggplot(dat, aes(x=occur, fill=app)) +
-    geom_histogram(binwidth=.5, alpha=.5, position="identity")
-}
+ggplot(combine, aes(x=occur, fill=app)) + geom_histogram(binwidth=.5, position="dodge")
+#ggplot(combine, aes(x=occur, fill=app)) + geom_density(alpha=.3)
